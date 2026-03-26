9be3eee6c911918d7a7cdeaadf1d0a9ac9e17f25
EugeneDokyeAnokye <yawanokye99@gmail.com>
2026-03-26 15:47:26 +0000

    Initialize ShiftSync project with Maven setup and basic application structure

---
+++ src/test/java/com/shiftsync/shiftsync/ShiftSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftsync.shiftsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
