0e2e9bec7efa5a5afd60393ee5f0249df920b243
EugeneDokyeAnokye <yawanokye99@gmail.com>
2026-04-24 18:06:55 +0000

    refactor(availability): clean up AvailabilityOverrideRepository by removing redundant annotations

--- src/main/java/com/shiftsync/shiftsync/availability/repository/AvailabilityOverrideRepository.java
+++ src/main/java/com/shiftsync/shiftsync/availability/repository/AvailabilityOverrideRepository.java
@@ -3,12 +3,8 @@ package com.shiftsync.shiftsync.availability.repository;
 import com.shiftsync.shiftsync.availability.entity.AvailabilityOverride;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AvailabilityOverrideRepository extends JpaRepository<AvailabilityOverride, Long> {
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
